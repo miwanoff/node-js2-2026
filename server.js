b6d12fd581dba0acb7714a4bbf5b30a692268db9
miwanoff <marina.iva.noffa@gmail.com>
2026-05-17 13:25:33 +0300

    Add route

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require("http");
-http
-  .createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("Hello World");
-    response.end();
-  })
-  .listen(8080);
+const url = require("url");
+
+
+function req(request, response) {
+  const base = url.parse(request.url);
+  const pathname = base.pathname;
+  console.log(pathname + " received" );
+  console.log(base);
+  response.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  response.write("Welcome");
+  response.write(pathname);
+  response.end();
+}
+http.createServer(req).listen(8080);
+console.log("Server starter");
