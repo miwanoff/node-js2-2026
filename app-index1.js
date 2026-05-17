59d8b807e08ffbbed13e8c8d578caf0a261b6ee1
miwanoff <marina.iva.noffa@gmail.com>
2026-05-17 13:06:32 +0300

    Add myfile.txt

--- app-index1.js
+++ app-index1.js
@@ -1,15 +1,19 @@
-const http = require('http');
-const fs = require('fs');
- 
-function req(request,response){
-    fs.readFile('index1.html',function(err,data){
-    response.writeHead(200,{
-        'Content-Type' : 'text/html'
+const http = require("http");
+const fs = require("fs");
+
+function req(request, response) {
+  fs.readFile("index1.html", function (err, data) {
+    if (err) {
+      console.error("Error reading file: " + err);
+      return;
+    }
+    response.writeHead(200, {
+      "Content-Type": "text/html",
     });
     response.write(data);
     response.end();
-    })
+  });
 }
- 
+
 http.createServer(req).listen(8080);
-console.log('Server starter');
+console.log("Server starter");
---
+++ err.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+
+fs.readFile('myfile.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file: ' + err);
+    return;
+  }
+  console.log('File content: ' + data);
+});
+
+console.log('Reading file... (this runs first!)');
